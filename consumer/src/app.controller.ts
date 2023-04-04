e5b9c6708a7e979a53d50007dc8efabd987da1d1
Juvenal Amarante <juvenalamarante13@gmail.com>
2023-04-04 12:22:52 -0300

    feat: add RabbitMq config in consumer

--- consumer/src/app.controller.ts
+++ consumer/src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { EventPattern, Payload } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +9,15 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @EventPattern('action_one')
+  actionOne(@Payload() data: any) {
+    console.log(`Message of Action One: ${data}`);
+  }
+
+  @EventPattern('action_two')
+  actionTwo(@Payload() data: any) {
+    console.log(`Message of Action Two: ${data}`);
   }
 }
--- consumer/src/main.ts
+++ consumer/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://localhost:5672'],
+        queue: 'producer_queue',
+        persistent: true,
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
